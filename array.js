a5eb6556a36f001c2d26dcfc70be5f1ed0e30ccf
German Smurov <smurovnemez@mail.ru>
2022-06-04 20:02:58 +0300

    Beginning-array

---
+++ array.js
@@ -0,0 +1,37 @@
+// два варинта для осздания пустого массива 
+let arr = new Array();
+let arr2 = ['german', '19', 'years']
+arr2[2] = 'old'
+
+
+
+
+
+console.log(arr2[0])
+console.log(arr2[1])
+console.log(arr2[2])
+console.log(arr2.length)
+console.log(arr2)
+
+
+let arr3 = ['apple', { name: 'Джон' }, true, function() { console.log(arr3); }];
+console.log(arr3)
+
+let fruits = [
+    "Яблоко",
+    "Апельсин",
+    "Слива",
+];
+
+let a = fruits.at(-1)
+console.log(a)
+
+
+function functionWithArgs(param1, param2) {
+    console.log(param1 + param2)
+}
+
+functionWithArgs(1, 2)
+
+
+// the rest freeCodeCamp
